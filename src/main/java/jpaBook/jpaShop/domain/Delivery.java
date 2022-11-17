2ded1ba523ef07eef03f8e9674c6b0b815ac76bc
gyunny <xorbs15923@naver.com>
2022-11-17 14:52:33 +0900

    jpa default -> 지연로딩 / 영속성 전이 적용

--- src/main/java/jpaBook/jpaShop/domain/Delivery.java
+++ src/main/java/jpaBook/jpaShop/domain/Delivery.java
@@ -2,6 +2,8 @@ package jpaBook.jpaShop.domain;
 
 import javax.persistence.*;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class Delivery extends BaseEntity {
 
@@ -13,6 +15,6 @@ public class Delivery extends BaseEntity {
     private String zipcode;
     private DeliveryStatus status;
 
-    @OneToOne(mappedBy = "delivery")
+    @OneToOne(mappedBy = "delivery", fetch = LAZY)
     private Order order;
 }
--- src/main/java/jpaBook/jpaShop/domain/OrderItem.java
+++ src/main/java/jpaBook/jpaShop/domain/OrderItem.java
@@ -1,6 +1,10 @@
 package jpaBook.jpaShop.domain;
 
+import com.zaxxer.hikari.util.ClockSource;
+
 import javax.persistence.*;
+
+import static javax.persistence.FetchType.LAZY;
 
 @Entity
 public class OrderItem extends BaseEntity {
@@ -9,11 +13,11 @@ public class OrderItem extends BaseEntity {
     @Column(name = "ORDER_ITEM_ID")
     private Long id;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ORDER_ID")
     private Order order;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "ITEM_ID")
     private Item item;
 
